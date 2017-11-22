e24319d6375ff120cbae9efa585ec670f4849026
Sarah Reiff <sbreiff@gmail.com>
2017-11-22 10:21:22 -0800

    Added escape to -# parameter in RAxML command

--- ePK_align.sh
+++ ePK_align.sh
@@ -13,4 +13,4 @@ trimal -in ePKs_named_hmmalign.fasta -out ePKs_named_hmmalign_trim.fasta -automa
 python run_sate.py sate_config.txt
 
 # raxml
-raxmlHPC-PTHREADS-AVX -T 16 -f a -p 12345 -s ePKs_sate/ePKs_sate.marker001.ePKs_named_hmmalign_trim.aln -x 12345 -# autoMRE -m PROTGAMMAAUTO -n ePKtree
+raxmlHPC-PTHREADS-AVX -T 16 -f a -p 12345 -s ePKs_sate/ePKs_sate.marker001.ePKs_named_hmmalign_trim.aln -x 12345 -\# autoMRE -m PROTGAMMAAUTO -n ePKtree
